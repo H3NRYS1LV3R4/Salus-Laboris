aaf1e44e560102fbe77c6020e81440dcba535b50
henry <henry.silvera.henry951@gmail.com>
2026-09-21 10:55:47 -0500

    configuracion inicial del proyecto Salus Laboris

---
+++ src/test/java/com/salusLaboris/salusLaboris/SalusLaborisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salusLaboris.salusLaboris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalusLaborisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
